4f98e6b3193679677536aee21d60f61d46507ac7
laviro <1ronlavi@gmail.com>
2019-06-12 14:19:05 +0300

    visual_regression_tests

--- webpack/assets/javascripts/react_app/components/LoginPage/__tests__/LoginPage.test.js
+++ webpack/assets/javascripts/react_app/components/LoginPage/__tests__/LoginPage.test.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import LoginPage from '../LoginPage';
 import { props } from '../LoginPage.fixtures';
 import { testComponentSnapshotsWithFixtures } from '../../../common/testHelpers';
@@ -5,8 +6,20 @@ import { testComponentSnapshotsWithFixtures } from '../../../common/testHelpers'
 const fixtures = {
   'renders LoginPage': props,
 };
-describe('AutoComplete', () => {
+describe('Login Page', () => {
   describe('rendering', () => {
     testComponentSnapshotsWithFixtures(LoginPage, fixtures);
   });
+
+  describe('visual regression', () => {
+    test('should render a Login Page', async () => {
+        await render(
+            <LoginPage {...props}></LoginPage>,
+            { viewport: { width: 100, height: 100 } }
+        );
+
+        const screenshot = await page.screenshot();
+        expect(screenshot).toMatchImageSnapshot();
+    });
+});
 });
